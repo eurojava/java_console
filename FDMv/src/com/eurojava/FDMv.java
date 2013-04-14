1483998f94ca37c6ea802355d113d2c9880fd89b
EuroJava <eurojava@spoko.pl>
2013-04-14 05:48:20 +0200

    FDMv ready to run

---
+++ FDMv/src/com/eurojava/FDMv.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eurojava;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FDMv {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        
+        File oldFile = new File("OldFile.txt");
+        oldFile.createNewFile();
+
+        File newFile = new File("NewFile.txt");
+
+        
+        oldFile.renameTo(newFile);
+    }
+}
