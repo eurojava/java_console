cd5aace9e80cd672d3d85e1852e131cbc66a9e2a
EuroJava <eurojava@spoko.pl>
2013-04-14 05:37:41 +0200

    CreateDirs ready to run

--- CreateDirs/src/com/eurojava/CreateDirs.java
+++ CreateDirs/src/com/eurojava/CreateDirs.java
@@ -13,11 +13,9 @@ public class CreateDirs {
      */
     public static void main(String[] args) {
  //
-
         String directories = "C:\\a\\b\\c\\d\\e\\f\\g\\h\\i";
         File file = new File(directories);
 
-        
         boolean result = file.mkdirs();
         System.out.println("Status = " + result);
     }
